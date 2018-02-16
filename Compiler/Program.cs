59cf1a15f3dabc68adc421880a62aae8fc81bc5e
MilanDierick <milan.dierick@gmail.com>
2018-02-16 17:30:48 +0100

    First commit.

---
+++ Compiler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.MSBuild;
+
+namespace Compiler
+{
+    static class Program
+    {
+        internal static void Main(string[] args)
+        {
+            string path = @"E:\dev\Projects\RoslynTest\RoslynTest\RoslynTest.sln";
+            var properties = new Dictionary<string, string>() {};
+            var msws = MSBuildWorkspace.Create(properties);
+
+            msws.WorkspaceFailed += (s, e) =>
+            {
+                Console.WriteLine($"Workspace failed with: {e.Diagnostic}");
+            };
+
+            var soln = msws.OpenSolutionAsync(path).Result;
+            var comp = soln.Projects.First(x => x.Name == "RoslynTest").GetCompilationAsync().Result;
+            var errs = comp.GetDiagnostics().Where(n => n.Severity == DiagnosticSeverity.Error).ToList();
+
+            comp.Emit(@"E:\dev\Projects\RoslynTest\RoslynTest\out\RoslynConsole.exe");
+
+            Console.WriteLine(".exe is created!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RoslynConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RoslynConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
